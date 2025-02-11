545cc9b6c7989c1544eb2f5df894aa325dcd2e86
Dupanshu <dupanshurangray@gmail.com>
2025-02-11 12:53:30 -0600

    slight changes in the design of the section1

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -3,24 +3,26 @@ import { BiCartAdd } from "react-icons/bi";
 
 function Section1(props) {
   return (
-    <section className="section1 container2">
-      <h1>Featured Collection</h1>
-      <div className="sec1-flex">
-        {props.flexItem.map(showItem =>(
-          <div key={showItem.id} className="sec1-flx-div">
-            <img src={`//unsplash.it/284/255`} alt='Pic1' title='Pic1' />
-            <div className="prod-info">
-              <div>
-                <p>{showItem.prodName}</p>
-                <p>{showItem.price}</p>
-              </div>
-              <div className="icn-flx">
-                <BiCartAdd />
-                <RiHeartAdd2Line />
+    <section className="section1">
+      <div className="container3">
+        <h1>Featured Collection</h1>
+        <div className="sec1-flex">
+          {props.flexItem.map(showItem =>(
+            <div key={showItem.id} className="sec1-flx-div">
+              <img src={`//unsplash.it/284/255`} alt='Pic1' title='Pic1' />
+              <div className="prod-info">
+                <div>
+                  <p>{showItem.prodName}</p>
+                  <p>{showItem.price}</p>
+                </div>
+                <div className="icn-flx">
+                  <BiCartAdd />
+                  <RiHeartAdd2Line />
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </section>
   );
