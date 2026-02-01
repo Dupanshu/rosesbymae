45a8eb58933378a05c32ffec54de95b0983b0a10
Dupanshu <dupanshurangray@gmail.com>
2026-02-01 16:50:47 -0600

    update

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,19 @@
+import about from '../media/about.jpg';
+
 function About() {
 
   return (
       <section className="about-page">
-        <div>
-          
-        </div>
+        <section className='flex space-around center'>
+          <div>
+            <h1></h1>
+            <p></p>
+            <button></button>
+          </div>
+          <div>
+            <img src={} alt='about' title='about' className="about-rgt-img"/>
+          </div>
+        </section>
       </section>
   );
 }
